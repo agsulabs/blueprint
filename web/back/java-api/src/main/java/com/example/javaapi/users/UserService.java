b91591e0605d47bc0fe58e2aefaad20909b43b71
Elvin Islamov <elvinaxsu@gmail.com>
2026-06-01 23:53:12 +0200

    Remove unused Java user repository injection

--- web/back/java-api/src/main/java/com/example/javaapi/users/UserService.java
+++ web/back/java-api/src/main/java/com/example/javaapi/users/UserService.java
@@ -10,10 +10,4 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
-
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 }
